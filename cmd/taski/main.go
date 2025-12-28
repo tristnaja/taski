5725fe3e5b89ae4b0d679bd42cd327a7c5deb401
tristan <tristan.alhabas@gmail.com>
2025-12-28 13:05:53 +0800

    feat: add `ChangeTask()` and `RemoveTask()`

--- cmd/taski/main.go
+++ cmd/taski/main.go
@@ -14,26 +14,26 @@ func main() {
 	fileName := "data.json"
 
 	task := io.Task{
-		Task:      "Test",
-		Date:      time.Now(),
-		IsDeleted: false,
+		Title:       "Test",
+		Description: "Halo",
+		Date:        time.Now(),
+		IsDeleted:   false,
 	}
 
 	task2 := io.Task{
-		Task:      "Test2",
-		Date:      time.Now(),
-		IsDeleted: false,
+		Title:       "Test2",
+		Description: "HaloHalo",
+		Date:        time.Now(),
+		IsDeleted:   false,
 	}
 
-	db := io.Database{}
-
-	err := io.AddTask(&db, &task, fileName)
+	err := io.AddTask(&task, fileName)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = io.AddTask(&db, &task2, fileName)
+	err = io.AddTask(&task2, fileName)
 
 	if err != nil {
 		log.Fatal(err)
@@ -46,4 +46,54 @@ func main() {
 	}
 
 	fmt.Println(result)
+
+	for index, value := range result.Tasks {
+		fmt.Printf("\n%d. %v\n", (index + 1), value.Title)
+		fmt.Printf("%v\n", value.Description)
+		fmt.Printf("Date: %v\n", value.Date)
+	}
+
+	err = io.ChangeTask(fileName, 0, "Title3", "")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = io.ChangeTask(fileName, 1, "Title4", "HaloHaloHalo")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err = io.ReadTask(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for index, value := range result.Tasks {
+		fmt.Printf("\n%d. %v\n", (index + 1), value.Title)
+		fmt.Printf("%v\n", value.Description)
+		fmt.Printf("Date: %v\n", value.Date)
+	}
+
+	err = io.RemoveTask(fileName, 1)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err = io.ReadTask(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println()
+
+	for index, value := range result.Tasks {
+		fmt.Printf("\n%d. %v\n", (index + 1), value.Title)
+		fmt.Printf("%v\n", value.Description)
+		fmt.Printf("Date: %v\n", value.Date)
+	}
 }
--- internal/io/io.go
+++ internal/io/io.go
@@ -2,15 +2,17 @@ package io
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"time"
 )
 
 type Task struct {
-	Task      string    `json:"task"`
-	Date      time.Time `json:"date"`
-	IsDeleted bool      `json:"is_deleted"`
+	Title       string    `json:"title"`
+	Description string    `josn:"description"`
+	Date        time.Time `json:"date"`
+	IsDeleted   bool      `json:"is_deleted"`
 }
 
 type Database struct {
@@ -18,7 +20,7 @@ type Database struct {
 	Tasks    []Task `json:"tasks"`
 }
 
-func AddTask(db *Database, task *Task, fileName string) error {
+func AddTask(task *Task, fileName string) error {
 	var jsonData []byte
 	var err error
 
@@ -27,6 +29,8 @@ func AddTask(db *Database, task *Task, fileName string) error {
 
 	// NOTE: if not nill (there IS an error), then there is no existing DB
 	if isDBExist != nil {
+		db := Database{}
+
 		db.Tasks = append(db.Tasks, *task)
 		db.Capacity++
 
@@ -34,19 +38,16 @@ func AddTask(db *Database, task *Task, fileName string) error {
 
 		jsonData, err = json.MarshalIndent(db, "", "\t")
 
-		if err != nil {
-			return fmt.Errorf("json indenting: %w", err)
-		}
 		// NOTE: if nill (there is NO error), then there is an existing DB
 	} else {
 		existingDB.Tasks = append(existingDB.Tasks, *task)
 		existingDB.Capacity++
 
 		jsonData, err = json.MarshalIndent(existingDB, "", "\t")
+	}
 
-		if err != nil {
-			return fmt.Errorf("json indenting: %w", err)
-		}
+	if err != nil {
+		return fmt.Errorf("json indenting: %w", err)
 	}
 
 	err = os.WriteFile(fileName, jsonData, 0644)
@@ -58,16 +59,70 @@ func AddTask(db *Database, task *Task, fileName string) error {
 	return nil
 }
 
-func ReadTask(fileName string) (Database, error) {
+func ReadTask(fileName string) (*Database, error) {
 	var result Database
 
 	message, err := os.ReadFile(fileName)
 
 	if err != nil {
-		return Database{}, fmt.Errorf("reading task: %w", err)
+		return &Database{}, fmt.Errorf("reading task: %w", err)
 	}
 
 	json.Unmarshal(message, &result)
 
-	return result, nil
+	return &result, nil
+}
+
+func ChangeTask(filename string, taskIndex int, newTitle string, newDescription string) error {
+	if newTitle == "" && newDescription == "" {
+		return errors.New("No value is changed")
+	}
+
+	db, err := ReadTask(filename)
+
+	if err != nil {
+		return fmt.Errorf("reading db: %w", err)
+	}
+
+	if newTitle == "" {
+		newTitle = db.Tasks[taskIndex].Title
+	}
+
+	if newDescription == "" {
+		newDescription = db.Tasks[taskIndex].Description
+	}
+
+	db.Tasks[taskIndex].Title = newTitle
+	db.Tasks[taskIndex].Description = newDescription
+	db.Tasks[taskIndex].Date = time.Now()
+
+	jsonData, err := json.MarshalIndent(db, "", "\t")
+
+	if err != nil {
+		return fmt.Errorf("reading db: %w", err)
+	}
+
+	err = os.WriteFile(filename, jsonData, 0644)
+
+	return nil
+}
+
+func RemoveTask(filename string, taskIndex int) error {
+	db, err := ReadTask(filename)
+
+	if err != nil {
+		return fmt.Errorf("reading db: %w", err)
+	}
+
+	db.Tasks = append(db.Tasks[:taskIndex], db.Tasks[taskIndex+1:]...)
+
+	jsonData, err := json.MarshalIndent(db, "", "\t")
+
+	if err != nil {
+		return fmt.Errorf("reading db: %w", err)
+	}
+
+	err = os.WriteFile(filename, jsonData, 0644)
+
+	return nil
 }
